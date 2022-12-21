b66dc8a638fdc4486793b3db89e5935581f9f0db
AhmedIAshraf <121035001+AhmedIAshraf@users.noreply.github.com>
2022-12-21 21:19:48 +0200

    Update Players_Time.c

--- Players_Time.c
+++ Players_Time.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <strings.h>
 #include <time.h>
+#include ""
 
 struct {
     char name[500];
@@ -17,24 +18,22 @@ void fun()
 
 int main()
 {
+    player1.color = blue();                   // Add The Header
+    player2.color = red();                     // Add The Header
+    
+    player1.score = 0; , player1.NumOfMoves =0;                 
+    player2.score = 0  , player2.NumOfMoves =0;              
+    
     printf("Player 1 Name : ");
     scanf ("%s",player1.name);
-    system("cls");
-
-    printf("Player 1 color : ");
-    scanf ("%s",player1.color);
     system("cls");
 
     printf("Player 2 Name : ");
     scanf ("%s",player2.name);
     system("cls");
-
-    printf("Player 2 color : ");
-    scanf ("%s",player2.color);
-    system("cls");
-
+    
     int start = clock();
-    fun();                           //Clock Only Works With Functions , Loops X
+    fun();                           //Clock Only Works With Functions , Loops X   ,,, The GameLoop Function
     int end = clock();
     int seconds = (end - start)/CLOCKS_PER_SEC;
     int minutes=0 , hours=0 ;
@@ -49,6 +48,12 @@ int main()
         minutes %= 60;
     }
     printf("The Game Took \n%d HH : %d MM : %d SS \n",hours ,minutes,seconds);
+    
+    printf("Player X Won , Great Job , And Now Please Enter Your Name : ");
+    gets(playerX);
+    printf("\nYour Highest Score Is &d",playerX.score);
+    
+    
     return 0;
 }
 
