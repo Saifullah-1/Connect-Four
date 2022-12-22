3440206c964c46ec312e0045e10d8ebe74a4b812
Saifullah Mosaad <109905783+Saifullah-1@users.noreply.github.com>
2022-12-22 19:07:05 +0200

    Add files via upload

---
+++ SaveAndLoad.c
@@ -0,0 +1,1 @@
+#include "SaveAndLoad.h"
---
+++ SaveAndLoad.h
@@ -0,0 +1,10 @@
+#ifndef Save_Load /*Header Guard*/
+#define Save_Load
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+#include <windows.h>
+#include "SaveAndLoad.c"
+
+#endif
