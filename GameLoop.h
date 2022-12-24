78810d5ab9092e0c31916a2403e7757030157bd3
Saifullah Mosaad <109905783+Saifullah-1@users.noreply.github.com>
2022-12-24 11:19:46 +0200

    Update GameLoop.h

--- GameLoop.h
+++ GameLoop.h
@@ -7,15 +7,22 @@
 #include <windows.h>
 #include "GameLoop.c"
 
+void SaveTop(char s[],int n);
+void Time (int start , int end);
+void Instant_Save(int i , int j);
+void TopScores(char name[] , int s);
 void resetPlayerTurnArr(void);
 void resetColumnFreeSpacesArr(void);
 void resetPlayerData(void);
 void resetBoard(void);
 void printBoard(void);
 int checkFreeSpaces(void);
-void checkColumnFreeSpaces(int column_index);
 void playerMove(void);
 void computerMove(void);
 void checkScore(void);
 void playerData(void);
+void checkWinner(void);
+void startGame(void);.
+int inGameMenu(void);
+
 #endif
