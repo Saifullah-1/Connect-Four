2a6bfd2fda99e21f31bdcf1df662f4317ebd7158
AhmedIAshraf <121035001+AhmedIAshraf@users.noreply.github.com>
2022-12-29 09:21:25 +0200

    Update SaveAndLoad.h

--- SaveAndLoad.h
+++ SaveAndLoad.h
@@ -7,5 +7,6 @@
 #include <windows.h>
 #include <time.h>
 #include <ctype.h>
-void file_save(int file_number);
+void file_save(int file_number,int r , int c , int position[r*c], int turn , int moves1 ,int moves2 ,int score1 ,int  score2 , char grid[r*c]);
 #endif
+void Load () ;
