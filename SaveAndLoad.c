2ff7ba9567e496fbc03f65ef980a1cf845ca890d
AhmedIAshraf <121035001+AhmedIAshraf@users.noreply.github.com>
2022-12-29 19:04:07 +0200

    Update SaveAndLoad.c

--- SaveAndLoad.c
+++ SaveAndLoad.c
@@ -1,7 +1,100 @@
 #include "SaveAndLoad.h"
 #include "SaveAndLoad.h"
 #include "GameLoop.c"
-void file_save(int file_number,int r , int c , int position[r*c], int turn , int moves1 ,int moves2 ,int score1 ,int  score2 , char grid[r*c])
+
+
+
+
+int info[5]={0};
+
+void file_save(int file_number,int r , int c , int position[r*c], int turn , int moves1 ,int moves2 ,int score1 ,int  score2 , char grid[r][c] , int t)
+{
+    if (file_number == 1)
+    {
+        FILE *p;
+        p = fopen("SaveGame1.txt","wb");
+        for (int i=0 ; i<r*c ; i++)
+        {
+                fwrite(&position[i],sizeof(position[i]),1,p);
+        }
+        fwrite(&moves1,sizeof(moves1),1,p);
+        fwrite(&moves2,sizeof(moves2),1,p);
+        fwrite(&score1,sizeof(score1),1,p);
+        fwrite(&score2,sizeof(score2),1,p);
+        fwrite(&turn,sizeof(turn),1,p);
+
+        for (int i=0 ; i<r ; i++)
+        {
+            for (int j=0 ; j<c ; j++)
+                fwrite(&grid[i][j],sizeof(grid[i][j]),1,p);
+        }
+
+
+
+        fclose (p);
+    }
+}
+
+void Load ()                     // Save File 1
+{
+    FILE* p;
+    p=fopen ("SaveGame1.txt","rb");
+    for (int i =0 ; i<rows*colmns ; i++)
+    {
+        fread(&position_array[i],sizeof(position_array[i]),1,p);
+    }
+
+    for (int i =0 ; i<5 ; i++)
+    {
+        fread (&info[i],sizeof(info[i]),1,p);
+    }
+
+    for (int i=0 ; i<rows ; i++)
+        {
+            for (int j=0 ; j<colmns ; j++)
+                fread(&board[i][j],sizeof(board[i][j]),1,p);
+        }
+
+    fclose(p);
+}
+
+
+
+/*
+void Load ()                     // Save File 1
+{
+    FILE *pr;
+    pr = fopen("SaveGame1.txt","r");
+        for (int i=0 ; i<rows*colmns ; i++)
+        {
+                fscanf(pr,"%d",&position_array[i]);
+        }
+        for (int i=0 ; i<rows ; i++)
+        {
+            for (int j=0 ; j<colmns ; j++)
+                fscanf(pr,"%c",&board[i][j]);
+        }
+        for (int i =0 ; i<5 ; i++)
+        {
+            fscanf(pr,"%d",info+1);
+        }
+
+        //fprintf(p,"%d",t);
+
+        fclose (pr);
+}
+*/
+
+
+
+
+
+
+
+/*
+int info[6]={0};
+
+void file_save(int file_number,int r , int c , int position[r*c], int turn , int moves1 ,int moves2 ,int score1 ,int  score2 , char grid[r*c] , int t)
 { // file number from save menu
     if (file_number == 1)
     {
@@ -21,6 +114,7 @@ void file_save(int file_number,int r , int c , int position[r*c], int turn , int
         fwrite(&score2,sizeof(score2),1, p);
 
         fwrite(&turn,sizeof(turn),1, p);
+        fwrite(&t,sizeof(t),1, p);
 
         fclose (p);
     }
@@ -42,6 +136,7 @@ void file_save(int file_number,int r , int c , int position[r*c], int turn , int
         fwrite(&score2,sizeof(score2),1, p);
 
         fwrite(&turn,sizeof(turn),1, p);
+        fwrite(&t,sizeof(t),1, p);
 
         fclose (p);
     }
@@ -63,6 +158,7 @@ void file_save(int file_number,int r , int c , int position[r*c], int turn , int
         fwrite(&score2,sizeof(score2),1, p);
 
         fwrite(&turn,sizeof(turn),1, p);
+        fwrite(&t,sizeof(t),1, p);
 
         fclose (p);
     }
@@ -82,12 +178,12 @@ void Load ()                     // Save File 1
     {
             fread(&board[i],sizeof(board[i]),1,pr);
     }
-    fwrite(&Player1.PlayerMoves,sizeof(Player1.PlayerMoves),1, pr);
-    fwrite(&Player2.PlayerMoves,sizeof(Player2.PlayerMoves),1, pr);
-    fwrite(&Player1.PlayerScore,sizeof(Player1.PlayerScore),1, pr);
-    fwrite(&Player2.PlayerScore,sizeof(Player2.PlayerScore),1, pr);
 
-    fwrite(&turn,sizeof(turn),1, pr);
+    for (int i=0 ; i<6 ; i++)
+    {
+            fread(&info[i],sizeof(info[i]),1,pr);
+    }
 
     fclose(pr);
 }
+*/
